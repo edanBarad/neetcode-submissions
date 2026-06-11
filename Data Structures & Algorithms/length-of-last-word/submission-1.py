d4b15df626e10ec1c2e0978abf429d7775508b3f
Edan Barad <edan@barad.org>
2026-06-11 22:47:40 +0300

    Remove: length-of-last-word - submission-1

--- Data Structures & Algorithms/length-of-last-word/submission-1.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        s.strip()
-        if s.find(" ") == -1: return len(s)
-        end = len(s)-1
-        while s[end] == ' ': end -= 1
-        start  = end
-        while s[start] != ' ' and start > 0: start -= 1
-        return end-start
